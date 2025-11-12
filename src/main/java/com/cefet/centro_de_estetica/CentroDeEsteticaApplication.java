3b6787591fd8665720ce2b305454ff624de8f626
FlavioCorcini <flaviosamora06@gmail.com>
2025-11-12 16:11:42 -0300

    first commit

---
+++ src/main/java/com/cefet/centro_de_estetica/CentroDeEsteticaApplication.java
@@ -0,0 +1,13 @@
+package com.cefet.centro_de_estetica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroDeEsteticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroDeEsteticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cefet/centro_de_estetica/CentroDeEsteticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefet.centro_de_estetica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroDeEsteticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
