00bd460b1a53b2f822b3b9117c9cc3cc805f30f1
vitorolivecardk <vitorolivecard@gmail.com>
2025-12-16 14:05:26 -0300

    Add route for AgendamentoClienteComponent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login'; // Importe seu componente
+import { AgendamentoClienteComponent } from './agendamento-cliente/agendamento-cliente'; 
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redireciona a raiz para o login
     { path: 'login', component: LoginComponent }          // Define a rota do login
+    { path: 'agendamento-cliente', component: AgendamentoClienteComponent }
 ];
